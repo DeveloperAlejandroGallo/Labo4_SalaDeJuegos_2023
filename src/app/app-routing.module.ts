f411d806bcb53190ca0fe5ec1ce22ae52963bca1
Alejandro Gallo <alegallo@gmail.com>
2023-05-11 20:07:51 -0300

    Sprint 1 Fin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './pages/error/error.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { QuienSoyComponent } from './pages/quien-soy/quien-soy.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login',component: LoginComponent },
   { path: 'home',component: HomeComponent },
   { path: 'error',component: ErrorComponent },
+  { path: 'quien-soy',component: QuienSoyComponent },
 
 
   {path: '**', pathMatch: 'full', redirectTo: 'error'}
